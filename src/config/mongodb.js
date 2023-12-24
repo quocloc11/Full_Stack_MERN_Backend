359ece112e40ce858e96d2d4dfcce4460f67909c
unknown <loc842211@gmail.com>
2023-12-24 11:22:23 +0700

    to chuc bien moi truong env

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -2,4 +2,41 @@
  * Updated by trungquandev.com's author on August 17 2023
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
+ * tranquocloc
+ * 183.80.38.229/32
+
+
+ * iIjaZd4xDMmAYVBr
  */
+
+
+
+import { env } from '~/config/environment'
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+
+let trelloDatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+})
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect()
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first!')
+  return trelloDatabaseInstance
+}
+
+
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * Updated by trungquandev.com's author on August 17 2023
  * YouTube: https://youtube.com/@trungquandev
@@ -5,28 +6,45 @@
  */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
+import 'dotenv/config'
+import { env } from '~/config/environment'
+const START_SERVER = () => {
+  const app = express()
 
-const app = express()
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-const hostname = 'localhost'
-const port = 8017
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello ${env.AUTHOR}, I am running at ${env.APP_HOST}:${env.APP_PORT}`)
+  })
+  exitHook(() => {
+    console.log('1Exit app')
+    CLOSE_DB()
+    console.log('Exit app')
+  })
+}
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+(async () => {
+  try {
+    console.log('1.Conned to MongoDB Cloud Atlas')
+    await CONNECT_DB()
+    console.log('2.Conned to MongoDB Cloud Atlas')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+// CONNECT_DB()
+//   .then(() => console.log('Conned to MongoDB Cloud Atlas'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
